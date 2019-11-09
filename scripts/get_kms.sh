0b6d33f51103e9485b88e03961bfaa16a5f3fcc8
HaoyuGong <33477685+HaoyuGong@users.noreply.github.com>
2019-11-09 13:54:05 +0800

    Update get_kms.sh

--- scripts/get_kms.sh
+++ scripts/get_kms.sh
@@ -1,10 +1,12 @@
-echo" Get KMS service"
+echo "Get KMS service"
 if [ -d "binaries" ]; then
 	echo "Delete the old version"
 	rm -r binaries
 fi
+
 sleep 2
 echo "Downloading files…"
+
 wget https://github.com/Wind4/vlmcsd/releases/download/svn1112/binaries.tar.gz
 
 tar -xvzf binaries.tar.gz
