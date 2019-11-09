1c6527d28fa9276342d77ed3fe80adafb2a3497e
HaoyuGong <33477685+HaoyuGong@users.noreply.github.com>
2019-11-09 16:19:07 +0800

    Update run_kms.sh

--- scripts/run_kms.sh
+++ scripts/run_kms.sh
@@ -26,3 +26,16 @@ elif [ processArch=="aarch64" ]; then
 fi
 sleep 1
 echo -e "KMS service is ready at\033[32m $HOST_IP:$port \033[0m"
+sleep 1
+echo -e "\033[31m Active Windows \033[0m"
+echo -e "\033[33m Config the KMS IP \033[0m"
+echo -e "\033[32m slmgr -skms $HOST_IP\033[0m"
+echo -e "\033[33m Operate active \033[0m"
+echo -e "\033[32m slmgr -ato \033[0m"
+echo -e "\033[31m Active Office \033[0m"
+echo -e "\033[33m Set path of office \033[0m"
+echo -e "\033[32m Set-Location 'C:\Program Files\Microsoft Office\Office16' \033[0m"
+echo -e "\033[33m Config the KMS IP \033[0m"
+echo -e "\033[32m cscript ospp.vbs /sethst:$HOST_IP \033[0m"
+echo -e "\033[33m Operate active \033[0m"
+echo -e "\033[32m cscript ospp.vbs /act \033[0m"
