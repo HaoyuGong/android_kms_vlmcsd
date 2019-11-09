faf18d1351535e7c23af4ca80d7a3550421c96e7
HaoyuGong <33477685+HaoyuGong@users.noreply.github.com>
2019-11-09 15:20:40 +0800

    Update stop_kms.sh

--- scripts/stop_kms.sh
+++ scripts/stop_kms.sh
@@ -1,9 +1,11 @@
 echo "Stop KMS service..."
 
 port=1688
-pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/"'{print $1}');
+pid=$(netstat -nlp | grep :$port | awk '{print $7}' | awk -F"/" '{ print $1 }');
 
 if [ -n "$pid" ]; then
-      kill -9 $pid
-      echo "Stop KMS service successful!!!"
+				kill -9 $pid
+				echo "Stop KMS service successful!!!"
+else
+				echo "KMS service has already shutdown!!!"
 fi
