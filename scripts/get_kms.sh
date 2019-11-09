1172a5f47f5650d6fad2d8517a48a51c5a288360
Lemon7ProPlus <33477685+Lemon7ProPlus@users.noreply.github.com>
2019-11-09 19:54:23 +0800

    Update get_kms.sh

--- scripts/get_kms.sh
+++ scripts/get_kms.sh
@@ -6,7 +6,7 @@ if [ -d "binaries" ]; then
 fi
 
 sleep 1
-echo "Downloading files…"
+echo "Downloading filesâ€¦"
 # Get the release from the Wind4's github
 wget https://github.com/Wind4/vlmcsd/releases/download/svn1112/binaries.tar.gz
 
